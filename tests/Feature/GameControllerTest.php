ab30c3b1d39abd158c1a3b98e6f8a5e5c56421a8
Flub78 <frederic.peignot@free.fr>
2021-05-26 21:44:33 +0200

    Refactoring

--- tests/Feature/GameControllerTest.php
+++ tests/Feature/GameControllerTest.php
@@ -27,38 +27,12 @@ class GameControllerTest extends TestCase {
 	function __destruct() {
 		$this->user->delete ();
 	}
-
-	/**
-	 * Index view
-	 *
-	 * @return void
-	 */
-	public function test_games_index() {
-		$this->be ( $this->user );
-		$response = $this->get ( '/games' );
-		$response->assertStatus ( 200 );
-		$response->assertSeeText ( 'Game Name' );
-		$response->assertSeeText ( 'Edit' );
-	}
-
-	/**
-	 * Create view
-	 *
-	 * @return void
-	 */
-	public function test_games_create() {
-		$this->be ( $this->user );
-		$response = $this->get ( '/games/create' );
-		$response->assertStatus ( 200 );
-		$response->assertSeeText ( 'Add Games Data' );
-	}
 	
 	/**
-	 * Edit view
-	 *
-	 * @return void
+	 * Create an element and returns its id
+	 * @return int
 	 */
-	public function test_games_edit() {
+	private function create_first() {
 		$this->be ( $this->user );
 		
 		$initial_count = Game::count();
@@ -72,11 +46,59 @@ class GameControllerTest extends TestCase {
 
 		# Read
 		$stored = Game::where('name', $game->name)->first();
-		$id = $stored->id;
+		return ($stored->id);		
+	}
+
+	/**
+	 * Index view
+	 *
+	 * @return void
+	 */
+	public function test_games_index_view() {
+		$this->be ( $this->user );
+		$response = $this->get ( '/games' );
+		$response->assertStatus ( 200 );
+		$response->assertSeeText ( 'Game Name' );
+		$response->assertSeeText ( 'Edit' );
+	}
+
+	/**
+	 * Create view
+	 *
+	 * @return void
+	 */
+	public function test_games_create_view() {
+		$this->be ( $this->user );
+		$response = $this->get ( '/games/create' );
+		$response->assertStatus ( 200 );
+		$response->assertSeeText ( 'Add Games Data' );
+	}
+	
+	/**
+	 * Edit view
+	 *
+	 * @return void
+	 */
+	public function test_games_edit_view_existing_element() {
+		
+		$id = $this->create_first();
 		
 		$response = $this->get ( "/games/$id/edit" );
 		$response->assertStatus ( 200 );
 		$response->assertSeeText ( 'Edit Game Data' );
 	}
 	
+	/**
+	 * Edit view
+	 *
+	 * @return void
+	 */
+	public function test_games_edit_view_unknown_element_return_404() {
+		
+		$id = $this->create_first() + 1000;
+		
+		$response = $this->get ( "/games/$id/edit" );
+		$response->assertStatus ( 404 );	// not found		
+	}
+		
 }
